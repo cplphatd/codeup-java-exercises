d5a19800b794d54d4eefb9d6f710d86af78a2959
David Ryan Alviola <david.alviola@gmail.com>
2017-01-20 09:37:39 -0600

    Refactored Roshambo enum to include static methods to determine results of a game.

--- src/RoshamboApp.java
+++ src/RoshamboApp.java
@@ -58,7 +58,7 @@ public class RoshamboApp {
             //Compare moves
             System.out.println("---");
             System.out.println(roshamboPlayer.getPlayerName()+ " threw: " + playerMove + ". CPU threw: " + opponentMove + ".");
-            determineResult(playerMove, opponentMove);
+            Roshambo.determineResult(playerMove, opponentMove);
 
             //Show score
             showScore();
@@ -84,82 +84,6 @@ public class RoshamboApp {
                 "|  |\\  \\----.|  `--'  | .----)   |   |  |  |  |  /  _____  \\  |  |  |  | |  |_)  | |  `--'  | \n" +
                 "| _| `._____| \\______/  |_______/    |__|  |__| /__/     \\__\\ |__|  |__| |______/   \\______/  \n" +
                 "                                                                                              ");
-    }
-
-    /**
-     * <p>Takes in string values of the player and CPU moves then calls the appropriate method depending
-     * on the result.</p>
-     *
-     * @param playerMove string value of the move the player threw
-     * @param opponentMove string value of the move the CPU threw
-     */
-    private static void determineResult (String playerMove, String opponentMove) {
-        switch (playerMove) {
-            case "rock":
-                switch (opponentMove) {
-                    case "paper":
-                        loss();
-                        break;
-                    case "scissors":
-                        win();
-                        break;
-                    default:
-                        draw();
-                }
-                break;
-            case "paper":
-                switch (opponentMove) {
-                    case "rock":
-                        win();
-                        break;
-                    case "scissors":
-                        loss();
-                        break;
-                    default:
-                        draw();
-                }
-                break;
-            case "scissors":
-                switch (opponentMove) {
-                    case "rock":
-                        loss();
-                        break;
-                    case "paper":
-                        win();
-                        break;
-                    default:
-                        draw();
-                }
-                break;
-            default:
-                System.out.println("Error @ determineResult");
-        }
-    }
-
-    /**
-     * <p>Displays message when player wins. Increases the number of session wins by 1.</p>
-     */
-    private static void win () {
-        System.out.println("---");
-        System.out.println("You win!");
-        RoshamboPlayer.incrementWins();
-    }
-
-    /**
-     * <p>Displays message when player loses. Increases the number of session losses by 1.</p>
-     */
-    private static void loss () {
-        System.out.println("---");
-        System.out.println("You lose...");
-        RoshamboPlayer.incrementLosses();
-    }
-
-    /**
-     * <p>Displays message when player and CPU draw.</p>
-     */
-    private static void draw () {
-        System.out.println("---");
-        System.out.println("Draw.");
     }
 
     /**
